50290c6ff95b5d668d97031a8a7f76c726acd9cb
Nimrandir <smhorowitz@earthlink.net>
2018-03-05 12:49:53 -0800

    Added documentation to .cpp and improved makefile

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,57 +1,74 @@
 #include "fibonacci.h"
 
-int cmdArgNum = 4; 
+/*
+ * Fibonacci is used to return the nth fibonacci number.
+ * Has two methods, "quick" and "slow"
+ * Uses mpz bigNum to handle calues larger than normal c++ data types
+ * Quick - uses multiplication of 2x2 matrices for very fast calculation
+ * Slow - traditional method using addition of previous values
+ * Usage: fibonacci fibNum printType fibType
+ * fibNum - the fibonacci number to be calculated
+ * printType - how to print (default is 0): 
+ *             0 only prints calculation time
+ *             1 prints fibonacci number with calculation and print time
+ *             2 only prints fibonacci number
+ * fibType - which calculation method to use (default is 0):
+ *                           0 for quick
+ *                           1 for slow
+ */
+
+int cmdArgNum = 4; // Function has 4 total args: function, fibNum, printType, fibType
 
 int main(int argc, char** argv)
 {
-    if(argc < 2 || argc > cmdArgNum){
+    if(argc < 2 || argc > cmdArgNum){ // Function requires at least  a number for calculation and can't take more that 3 external args
         std::cout << "Invalid number of inputs." << std::endl;
         return 1;
     }
     int fibNum;
-    int doPrint = 0;
-    int fibType = 0;
-    std::istringstream iss(argv[1]);
+    int printType = 0; // Default is to only do calculation and print time taken
+    int fibType = 0; // Default is quick method
+    std::istringstream iss(argv[1]); // Used to convert command line argument to string
     if(iss >> fibNum && fibNum > 0){} //Sets fibNum as first command line argument if valid
     else{
         std::cout << "Invalid first argument. Expecting positive integer." << std::endl;
-        return 1;
+        return 1; // Returns with error if argument is invalid
     }
-    if(argc > 2){
-        std::istringstream bss(argv[2]);
-        if(bss >> doPrint){} //Sets doPrint as second command line argument if valid
+    if(argc > 2){ // Only try to set print type if there are enough arguments
+        std::istringstream bss(argv[2]); // Used to convert command line argument to string
+        if(bss >> printType){} //Sets printType as second command line argument if valid
         else{
             std::cout << "Invalid second argument. Expecting integer." << std::endl;
-            return 1;
+            return 1; // Returns with error if argument is invalid
         }
     }
     if(argc > 3){
-        std::istringstream bss(argv[3]);
+        std::istringstream bss(argv[3]); // Used to convert command line argument to string
         if(bss >> fibType){} //Sets fibType as third command line argument if valid
         else{
             std::cout << "Invalid third argument. Expecting integer." << std::endl;
-            return 1;
+            return 1; // Returns with error if argument is invalid
         }
     }
-    mpz_t result;
-    mpz_init(result);
-    time_t calcTimerInit;
-    time_t calcTimerEnd;
+    mpz_t result; // Defines big integer for holding fibonacci value
+    mpz_init(result); // mpz values must be initialized
+    time_t calcTimerInit; //    
+    time_t calcTimerEnd;  // Times used to figure out calculation time of function
     time(&calcTimerInit);
-    if(fibType)
+    if(fibType) // Performs slow calculation if fibType is 1, quick calculation otherwise
         fibSlow(fibNum, result);
     else
         fibQuick(fibNum, result);
     time(&calcTimerEnd);
-    if(doPrint){
-        if(doPrint == 2){
+    if(printType){ // If printType is 0, only prints calculation time
+        if(printType == 2){
             std::cout << result << std::endl;
-            return 0;
+            return 0; // Done after printing result of calculation
         }
         time_t printTimerInit;
         time_t printTimerEnd;
         int lastDigit = fibNum % 10;
-        switch(lastDigit){
+        switch(lastDigit){ // Properly numerates which fibonacci number is being calculated
             case 1: std::cout << fibNum << "st "; break;
             case 2: std::cout << fibNum << "nd "; break;
             case 3: std::cout << fibNum << "rd "; break;
--- makefile
+++ makefile
@@ -1,6 +1,17 @@
 fibNum=1000000
 fib.out:
+ifeq (,$(wildcard /usr/local/include/gmpxx.h))
+	make gmpGet
+endif
 	g++ fibonacci.cpp -o fib.out -lgmpxx -lgmp
+
+gmpGet:
+ifeq (,$(wildcard gmp-6.1.2.tar.xz))
+	wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.xz 
+endif
+	tar -xf gmp-6.1.2.tar.xz
+	cd gmp-6.1.2 && ./configure --enable-cxx && sudo make && sudo make install && sudo make check
+	sudo rm -r gmp*
 
 test: fib.out
 	./fib.out>fibQuick $(fibNum) 2
@@ -8,5 +19,11 @@ test: fib.out
 	diff -s fibQuick fibSlow
 	rm fibQuick fibSlow
 
-clear:
+fibQuick: fib.out
+	./fib.out $(fibNum)
+
+fibSlow: fib.out
+	./fib.out $(fibNum) 0 1
+
+clean:
 	rm *.out
